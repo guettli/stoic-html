4ff648df290a3bc2624e81be1ec82ffcd38776fe
Thomas Guettler <guettliml@thomas-guettler.de>
2021-08-18 21:12:48 +0200

    .

--- stoic_html/tests/test_stoic_html.py
+++ stoic_html/tests/test_stoic_html.py
@@ -6,7 +6,6 @@ from stoic_html import (
 
 
 def test_join():
-    assert 0
     assert join(['<', '>', '&'], ',') == '&lt;,&gt;,&amp;'
     assert join([1, 2, 3]) == '123'
     assert join([]) == ''
